20db6e1b511b6b4dfef23a3617979cbc74612de5
vDawgg <veit.laule@gmail.com>
2021-11-28 14:40:05 +0100

    Added progress bars to technical section, website now complete!

--- script.js
+++ script.js
@@ -26,23 +26,41 @@ function showSlides(i) {
 }
 
 function showLanguages() {
-    if("none"==$("#java").css("display")) {
+    if("none"==$(".progressWrapperLanguages").css("display")) {
         $("#languages").text("Languages \u25B3");
-        $("#java").css("display", "block");
-        $("#java").addClass("widen");
-        console.log("oi");
+        $(".progressWrapperLanguages").css("display", "block");
+        $("#java").css({"width":"300px", "background":"red"})
+        $("#js").css({"width":"200px", "background":"orange"});
+        $("#python").css({"width":"150px", "background":"orange"});
     } else {
         $("#languages").text("Languages \u25BD");
-        $("#java").css("display", "none");
-        $("#java").removeClass("widen");
-        console.log("ey");
+        $(".progressWrapperLanguages").css("display", "none");
     }
 }
 
 function showIDEs() {
-    $("#ides").text("IDEs \u25B3");
+    if("none"==$(".progressWrapperIdes").css("display")) {
+        $("#ides").text("IDEs \u25B3");
+        $(".progressWrapperIdes").css("display", "block");
+        $("#intellij").css({"width":"280px", "background":"red"});
+        $("#vscode").css({"width":"250px", "background":"red"});
+        $("#eclipse").css({"width":"100px", "background":"yellow"});
+    } else {
+        $("#ides").text("IDEs \u25BD");
+        $(".progressWrapperIdes").css("display", "none");
+    }
 }
 
 function showOther() {
-    $("#other").text("Other \u25B3");
+    if("none"==$(".progressWrapperOther").css("display")) {
+        $("#other").text("Other \u25B3");
+        $(".progressWrapperOther").css("display", "block");
+        $("#linux").css({"width":"250px", "background":"red"});
+        $("#spring").css({"width":"230px","background":"orange"});
+        $("#docker").css({"width":"180px", "background":"orange"});
+        $("#react").css({"width":"130px", "background":"yellow"});
+    } else {
+        $("#other").text("Other \u25BD");
+        $(".progressWrapperOther").css("display", "none");
+    }
 }
