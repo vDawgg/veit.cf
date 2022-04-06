801bafa5d4aba0568194078de8c6002f679ef7ca
vDawgg <veit.laule@gmail.com>
2022-04-06 16:32:25 +0000

    Updated cv and main page text, changed photos (should load better now)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var slides = [];
 $(document).ready(function onLoad() {
     var src = "/pics/slideshow/";
     var i = 0;
-    for(i; i<12; i++) { //api?
+    for(i; i<10; i++) { //api?
         slides.push(src+i+".jpg");
     }
     $(".numbertext").text(1+"/"+slides.length);
@@ -27,6 +27,14 @@ function showSlides(i) {
 
 function showLanguages() {
     if("none"==$(".progressWrapperLanguages").css("display")) {
+        if("block"==$(".progressWrapperOther").css("display")) {
+            $("#other").text("Other \u25BD");
+            $(".progressWrapperOther").css("display", "none");
+        }
+        if("block"==$(".progressWrapperIdes").css("display")) {
+            $("#ides").text("IDEs \u25BD");
+            $(".progressWrapperIdes").css("display", "none");
+        }
         $("#languages").text("Languages \u25B3");
         $(".progressWrapperLanguages").css("display", "block");
         $("#java").css({"width":"300px", "background":"red"})
@@ -40,6 +48,14 @@ function showLanguages() {
 
 function showIDEs() {
     if("none"==$(".progressWrapperIdes").css("display")) {
+        if("block"==$(".progressWrapperLanguages").css("display")) {
+            $("#languages").text("Languages \u25BD");
+            $(".progressWrapperLanguages").css("display", "none");
+        }
+        if("block"==$(".progressWrapperOther").css("display")) {
+            $("#other").text("Other \u25BD");
+            $(".progressWrapperOther").css("display", "none");
+        }
         $("#ides").text("IDEs \u25B3");
         $(".progressWrapperIdes").css("display", "block");
         $("#intellij").css({"width":"280px", "background":"red"});
@@ -53,11 +69,21 @@ function showIDEs() {
 
 function showOther() {
     if("none"==$(".progressWrapperOther").css("display")) {
+        if("block"==$(".progressWrapperLanguages").css("display")) {
+            $("#languages").text("Languages \u25BD");
+            $(".progressWrapperLanguages").css("display", "none");
+        }
+        if("block"==$(".progressWrapperIdes").css("display")) {
+            $("#ides").text("IDEs \u25BD");
+            $(".progressWrapperIdes").css("display", "none");
+        }
         $("#other").text("Other \u25B3");
         $(".progressWrapperOther").css("display", "block");
         $("#linux").css({"width":"250px", "background":"red"});
+        $("#grpc").css({"width":"230px", "background":"orange"});
         $("#spring").css({"width":"230px","background":"orange"});
         $("#docker").css({"width":"180px", "background":"orange"});
+        $("#kubernetes").css({"width":"160px", "background":"orange"});
         $("#react").css({"width":"130px", "background":"yellow"});
     } else {
         $("#other").text("Other \u25BD");
